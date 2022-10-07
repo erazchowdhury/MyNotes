3bac89322281dfcba722f3209e46059ec438ed39
erazchowdhury <erazchow@gmail.com>
2022-10-07 03:53:05 -0400

    exceptions

---
+++ src/exception_handling/LearnExceptionHandling.java
@@ -0,0 +1,39 @@
+package exception_handling;
+
+public class LearnExceptionHandling {
+
+    public static void main(String[] args) {
+
+        String str = "JaCa is an IslAnd";
+        char[] strArr = str.toLowerCase().toCharArray();
+        int length = strArr.length;
+
+        try {
+            System.out.println(10/0);
+        } catch (Exception e) {
+
+        }
+
+        for (int i = 0; i <= length; i++) {
+            try {
+                System.out.println(strArr[i]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            } catch (IndexOutOfBoundsException e2) {
+                e2.getCause();
+            }
+        }
+
+        LearnExceptionHandling obj = new LearnExceptionHandling();
+        obj.divide(10,0);
+
+    }
+
+    public int divide(int num1, int num2) throws ArithmeticException{
+        if (num2 == 0) {
+            throw new ArithmeticException();
+        }
+        return num1 / num2;
+    }
+
+}
